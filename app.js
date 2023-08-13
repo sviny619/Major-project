a52f7b64ec544c7f65c85e5ec803c1e9324033f0
ShivaniYS <87171146+ShivaniYS@users.noreply.github.com>
2023-08-13 14:33:04 +0530

    code for displaying graph

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
-const express = require("express")
-const fs = require('fs');
-const jsdom = require('jsdom');
+const express = require("express");
+const fs = require("fs");
+const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const csvFilePath = 'public/trial_1.csv';
+const csvFilePath = "public/trial_1.csv";
 
-app = express()
+app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html")
-})
-app.post('/load', function (req, res) {
-    res.redirect('/plotGraph');
-})
-app.get('/plotGraph', function (req, res) {
-    fs.readFile(csvFilePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading the file:', err);
-            return;
-        }
-        const lines = data.trim().split('\n');
-        const xValues = [];
-        const yValues = [];
+  res.sendFile(__dirname + "/index.html");
+});
+app.post("/load", function (req, res) {
+  res.redirect("/plotGraph");
+});
+app.get("/plotGraph", function (req, res) {
+  fs.readFile(csvFilePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading the file:", err);
+      return;
+    }
+    const lines = data.trim().split("\n");
+    // const xValues = [];
+    const yValues = [];
 
-        lines.slice(1).forEach(line => {
-            const columns = line.split(',');
-            xValues.push(columns[3]); // Assuming 4th column is the x axis
-            yValues.push(columns[2]); // Assuming 3rd column is the y axis
-        });
-        
-
-        // Process the CSV data (you can replace this with your own logic)
-        console.log(xValues);
+    lines.slice(1).forEach((line) => {
+      const columns = line.split(",");
+      // xValues.push(columns[0]); // Assuming 4th column is the x axis
+      const yValue = parseFloat(columns[2]);
+      yValues.push(yValue); // Assuming 3rd column is the y axis
     });
-    res.send("hello world ")
-})
+    const xValues = Array.from(
+      { length: yValues.length },
+      (_, index) => index + 1
+    );
+    // Process the CSV data (you can replace this with your own logic)
+    console.log(xValues);
+    res.send({ xValues, yValues });
+  });
+});
 app.listen(3000, function () {
-    console.log("server is running on port 3000")
-})
+  console.log("server is running on port 3000");
+});
--- script.js
+++ script.js
@@ -1,25 +1,83 @@
-document.getElementById("loadButton").addEventListener("click", function() {
-    // Simulate loading the CSV file (replace this with actual code to load the CSV)
-    const csvData = "Name,Age,Location\nJohn,25,New York\nJane,30,Los Angeles";
+document.getElementById("loadButton").addEventListener("click", function () {
+  // Simulate loading the CSV file (replace this with actual code to load the CSV)
+  const csvData = "Name,Age,Location\nJohn,25,New York\nJane,30,Los Angeles";
 
-    // Split CSV data into rows
-    const rows = csvData.split('\n');
+  // Split CSV data into rows
+  const rows = csvData.split("\n");
 
-    // Create an HTML table to display the CSV data
-    const table = document.createElement('table');
-    for (let i = 0; i < rows.length; i++) {
-        const cells = rows[i].split(',');
-        const row = document.createElement('tr');
-        for (let j = 0; j < cells.length; j++) {
-            const cell = document.createElement(i === 0 ? 'th' : 'td');
-            cell.textContent = cells[j];
-            row.appendChild(cell);
-        }
-        table.appendChild(row);
+  // Create an HTML table to display the CSV data
+  const table = document.createElement("table");
+  for (let i = 0; i < rows.length; i++) {
+    const cells = rows[i].split(",");
+    const row = document.createElement("tr");
+    for (let j = 0; j < cells.length; j++) {
+      const cell = document.createElement(i === 0 ? "th" : "td");
+      cell.textContent = cells[j];
+      row.appendChild(cell);
     }
+    table.appendChild(row);
+  }
 
-    // Replace existing data with the new table
-    const csvContainer = document.getElementById('csvData');
-    csvContainer.innerHTML = '';
-    csvContainer.appendChild(table);
+  // Replace existing data with the new table
+  const csvContainer = document.getElementById("csvData");
+  csvContainer.innerHTML = "";
+  csvContainer.appendChild(table);
+
+  fetch("/plotGraph")
+    .then((response) => response.json())
+    .then((data) => {
+      const xValues = data.xValues.map((value) => parseFloat(value));
+      const yValues = data.yValues.map((value) => parseFloat(value));
+
+      // Create an HTML table to display the CSV data
+      const table = document.createElement("table");
+      for (let i = 0; i < xValues.length; i++) {
+        const row = document.createElement("tr");
+        const xCell = document.createElement("td");
+        xCell.textContent = xValues[i];
+        row.appendChild(xCell);
+        const yCell = document.createElement("td");
+        yCell.textContent = yValues[i];
+        row.appendChild(yCell);
+        table.appendChild(row);
+      }
+      const csvContainer = document.getElementById("csvData");
+      csvContainer.innerHTML = "";
+      csvContainer.appendChild(table);
+      const ctx = document.getElementById("myChart").getContext("2d");
+      new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: xValues,
+          datasets: [
+            {
+              label: "Y-axis Data",
+              data: yValues,
+              borderColor: "rgba(75, 192, 192, 1)",
+              fill: false,
+            },
+          ],
+        },
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              type: "linear",
+              position: "bottom",
+              title: {
+                display: true,
+                text: "X Values",
+              },
+            },
+            y: {
+              title: {
+                display: true,
+                text: "Y Values",
+              },
+            },
+          },
+        },
+      });
+    })
+    .catch((error) => console.error("Error fetching or plotting data:", error));
 });
