c5849c3c1e8d7f54b5a9159fd01ac4ed4eae2fcd
Vinny <nvins619@gmail.com>
2023-08-13 20:13:03 +0530

    added grqph feature

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
+const nodeplotlib = require('nodeplotlib');
 const csvFilePath = "public/trial_1.csv";
 
 app = express();
@@ -35,7 +36,39 @@ app.get("/plotGraph", function (req, res) {
     );
     // Process the CSV data (you can replace this with your own logic)
     console.log(xValues);
-    res.send({ xValues, yValues });
+    const trace = {
+      x: xValues,
+      y: yValues,
+      type: 'scatter'
+    };
+
+    const layout = {
+      title: 'My Plot',
+      xaxis: {
+        title: 'X-axis'
+      },
+      yaxis: {
+        title: 'Y-axis'
+      }
+    };
+
+    nodeplotlib.plot([trace], layout, (err, fig) => {
+      if (err) {
+          console.error('Error plotting graph:', err);
+          return res.status(500).send('Error plotting graph');
+      }
+
+      const buffer = fig.renderSync({ format: 'png' });
+      
+      res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+      res.set('Pragma', 'no-cache');
+     
+      res.set('Expires', '0');
+      res.set('Content-Type', 'image/png');
+
+      res.send(buffer);
+  });
+    
   });
 });
 app.listen(3000, function () {
